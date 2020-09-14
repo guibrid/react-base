073ec5bef8dddfec85b3b4a5c3037ba0343a48e2
Guillaume Briard <gbriard@lillebonne.hdv>
2020-09-14 14:42:54 +0200

    Updates

---
+++ precache-manifest.1b7d638d98c75f86f2c68fd711aea753.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6effc20ba632db9510e9007925c29635",
+    "url": "/react-base/index.html"
+  },
+  {
+    "revision": "7a66bef2cbbfc3064c22",
+    "url": "/react-base/static/css/main.cfa66741.chunk.css"
+  },
+  {
+    "revision": "cc80e00e39b0291f3e1e",
+    "url": "/react-base/static/js/2.1e334016.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-base/static/js/2.1e334016.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a66bef2cbbfc3064c22",
+    "url": "/react-base/static/js/main.43c96fc1.chunk.js"
+  },
+  {
+    "revision": "d3e3798b11ee117d4aed",
+    "url": "/react-base/static/js/runtime-main.1df645b8.js"
+  }
+]);
